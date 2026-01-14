b8ca77fec0970d28ae21b71dc697235d8ca5a7b7
Fractal Game Studios <contacto@fractalgamestudios.es>
2026-01-14 21:34:41 +0100

    FGS | Hytale template

---
+++ src/main/java/com/fractalgs/Main.java
@@ -0,0 +1,18 @@
+package com.fractalgs;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    public void setup() {
+        // Plugin setup code goes here
+    }
+}
